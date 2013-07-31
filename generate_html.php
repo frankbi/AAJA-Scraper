1621c1d29ee6c0ec3a5a4ec33db16f7115a195bf
Frank <frank@frankbi.com>
2013-07-31 00:46:19 -0400

    bleeeee

--- generate_html.php
+++ generate_html.php
@@ -1,28 +1,50 @@
 <?php
 	$file = fopen("nyc/Farmers_Markets_in_New_York_State.csv", "r");
-	$f = fopen("index.html", "a");
-	
+
+
+	$counter = 0;
+	$pagenum = 0;
+	$f;
 	while (($contents = fgetcsv($file)) != FALSE) {
-		fwrite($f, "<tr><td>" . $contents[0] . "</td>");
-		fwrite($f, "<td>" . $contents[1] . "</td>");
-		fwrite($f, "<td>" . $contents[3] . "</td>");
-		fwrite($f, "<td>" . $contents[4] . "</td>");
-		fwrite($f, "<td>" . $contents[5] . "</td>");
-		fwrite($f, "<td>" . $contents[6] . "</td>");
-		fwrite($f, "<td>" . $contents[13] . "</td>");
-		fwrite($f, "<td>" . $contents[14] . "</td></tr>\n");
+		$counter++;
+		if (($counter % 100) == 0) {
+			$pagenum++;
+			$f = fopen("page" . $pagenum . ".html", "w");
+			fwrite($f, "<html>\n<head>\n" . 
+				"    <title>Farmer Markets in New York</title>\n" .
+				"</head>\n<body>\n" . 
+				"    <table>\n");
+			writerow($counter, $f, $contents);
+		} else {
+			$pagenum++;
+			writerow($counter, $f, $contents);
+		}
 	}
-	
-	/*
-		County [0]
-		Market Name [1]
-		Address Line 1 [3]
-		City [4]
-		State [5]
-		Zip [6]
-		Latitude [13]
-		Longitude [14]
-	*/
+	fwrite($f, "    </table>\n</body>\n</html>");
+
+	function writerow($c, $file, $data) {
+		if ($c != 1) {
+			fwrite($file, "    <tr>\n        <td>" . $data[0] . "</td>\n    " .
+				"    <td>" . $data[1] . "</td>\n    " .
+				"    <td>" . $data[3] . "</td>\n    " .
+				"    <td>" . $data[4] . "</td>\n    " .
+				"    <td>" . $data[5] . "</td>\n    " .
+				"    <td>" . $data[6] . "</td>\n    " .
+				"    <td>" . $data[13] . "</td>\n    " .
+				"    <td>" . $data[14] . "</td>\n    " .
+				"</tr>\n");
+		} else {
+			fwrite($file, "    <th>\n        <td>" . $data[0] . "</td>\n    " .
+				"    <td>" . $data[1] . "</td>\n    " .
+				"    <td>" . $data[3] . "</td>\n    " .
+				"    <td>" . $data[4] . "</td>\n    " .
+				"    <td>" . $data[5] . "</td>\n    " .
+				"    <td>" . $data[6] . "</td>\n    " .
+				"    <td>" . $data[13] . "</td>\n    " .
+				"    <td>" . $data[14] . "</td>\n    " .
+				"</th>\n");
+		}
+	}
 
 	fclose($f);
 	fclose($file);
