53975d6b657a34c79bae59bf913f2027f540d577
danhillreports <danhillreports@gmail.com>
2013-08-09 23:36:39 -0400

    scraper w html.parser, still need to save

---
+++ scraper.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+ 
+import scraperwiki
+import requests
+
+from bs4 import BeautifulSoup
+ 
+base_url = 'http://frankbi.com/aaja/farmermarkets/'
+page_array = []
+data_array = []
+
+def get_pages():
+    html = requests.get(base_url+'page5.html')
+    soup = BeautifulSoup(html.content, "html.parser")
+    
+    page_list = soup.findAll(id='pages')
+    pages = page_list[0].findAll('a')
+    
+    for page in pages:
+        page_array.append(page.get('href'))
+
+def scrape_page(page):
+    print page
+    html = requests.get(page)
+    soup = BeautifulSoup(html.content, "html.parser")
+        
+        
+    table = soup.findAll('table')
+    rows = table[0].findAll('tr')
+
+    if len(soup.findAll('th')) > 0:
+        rows = rows[1:]
+
+    for row in rows:
+        cells = row.findAll('td')
+        data = {
+            'county' : cells[0].get_text(),
+            'market' : cells[1].get_text(),
+            'address' : cells[2].get_text(),
+            'city' : cells[3].get_text(),
+            'state' : cells[4].get_text(),
+            'zip_code' : cells[5].get_text(),
+            'lat' : cells[6].get_text(),
+            'lon' : cells[7].get_text()
+        }
+        # print data
+        data_array.append(data)
+
+
+get_pages()
+[scrape_page(base_url + page) for page in page_array]
+print len(data_array)
+
+# Saving data:
+# unique_keys = [ 'id' ]
+# data = { 'id':12, 'name':'violet', 'age':7 }
+# scraperwiki.sql.save(unique_keys, data)
